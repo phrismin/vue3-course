6418a78bbc983f9623afb61a4b350028a15ddf75
Рудой Алексей Юрьевич <rudoy@NLA.LOCAL>
2022-05-06 17:50:27 +0300

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,84 @@
 <template>
-  <div>
-    <div>Количество лайков: {{ likes }}</div>
+  <div class="app">
+    <form>
+      <h4>Создание поста</h4>
+      <input
+          class="input"
+          type="text"
+          name="name"
+          placeholder="Название">
+      <input
+          class="input"
+          type="text"
+          name="description"
+          placeholder="Описание">
+      <button class="btn" @click="createPost">Создать</button>
+    </form>
+    <div class="post" v-for="post in posts">
+      <div><strong>Название:</strong> {{ post.title }}</div>
+      <div><strong>Описание:</strong> {{ post.body }}</div>
+    </div>
   </div>
+
 </template>
 
 <script>
 export default {
   data() {
     return {
-      likes: 0,
+      posts: [
+        {id: 1, title: 'JavaScript1', body: 'Oписание поста'},
+        {id: 2, title: 'JavaScrip2', body: 'Oписание поста'},
+        {id: 3, title: 'JavaScript3', body: 'Oписание поста'},
+        {id: 4, title: 'JavaScrip4', body: 'Oписание поста'}
+      ],
+      title: '',
+      body: ''
+    }
+  },
+  methods: {
+    createPost() {
+
     }
   }
 }
 </script>
 
 <style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+.app {
+  padding: 20px;
+}
+
+.post {
+  padding: 15px;
+  border: 2px solid greenyellow;
+  margin-top: 15px;
+}
+
+.input {
+  width: 100%;
+  border: 1px solid darkgreen;
+  padding: 10px 15px;
+  margin-top: 15px;
+}
+
+.btn {
+  margin-top: 10px;
+  padding: 10px;
+  align-self: flex-end;
+  background: none;
+  color: teal;
+  border: 2px solid teal;
+}
 </style>
